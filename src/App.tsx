7f5e7d80b5fb9b0b4d34c6b656fe9a5fe19fb062
himanoa <matsunoappy@gmail.com>
2024-08-15 20:24:13 +0900

    Implemented converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import classes from './App.module.css'
+import { Alignment, Colors, Navbar, NavbarGroup, NavbarHeading } from '@blueprintjs/core'
+import { Converter } from './Converter'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={classes.page}>
+      <header>
+        <Navbar style={{ background: Colors.DARK_GRAY4, color: Colors.WHITE, padding: '8px' }}>
+          <NavbarGroup align={Alignment.LEFT}>
+            <NavbarHeading>Dungeonborne PatchNote to Wiki変換君</NavbarHeading>
+          </NavbarGroup>
+        </Navbar>
+      </header>
+      <section>
+        <h2>これなに？</h2>
+        <p><a href="https://dungeonborne.fandom.com/ja/wiki/%E3%83%80%E3%83%B3%E3%82%B8%E3%83%A7%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%B3_Wiki">ダンジョンボーンwiki</a>をメンテナンスするにあたって、パッチノートの文章を追加する必要があります。</p>
+        <p>しかし、パッチノートの文書はMarkdownで書かれていてwikiに転記するのが大変です。</p>
+        <p>このツールでは、Markdownの記法をwikitextに変換して簡単にパッチノートを追加できるようにするツールです。</p>
+        <h2>使い方</h2>
+        <p>左側のテキストエリアにDiscordのパッチノートをコピーしたものを入力すると右側にwikitext形式に変換したものが出力されます。</p>
+        <Converter />
+      </section>
+    </div>
   )
 }
 
---
+++ src/Converter.tsx
@@ -0,0 +1,30 @@
+import { ChangeEventHandler, useCallback, useState } from 'react'
+import classes from "./Converter.module.css"
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import { toWikiPlugin } from './mdast-to-wiki'
+
+const processor = unified().use(remarkParse).use(toWikiPlugin).freeze()
+
+export function Converter() {
+  const [raw, updateRaw] = useState("")
+  const [wikiText, updateWikiText] = useState("")
+
+  const handleChangeRaw = useCallback<ChangeEventHandler<HTMLTextAreaElement>>((e) => {
+    updateRaw(e.target.value)
+
+    processor.process(e.target.value).then((value) => {
+      return String(value)
+    }).then(processed => {
+      updateWikiText(processed)
+    })
+  }, [])
+
+  return (
+    <div className={classes.container}>
+      <textarea style={{width: "50%", height: "700px"}} onChange={handleChangeRaw} defaultValue={raw}></textarea>
+      <span>→</span>
+      <textarea style={{width: "50%", height: "700px"}} disabled defaultValue={wikiText}></textarea>
+    </div>
+  )
+}

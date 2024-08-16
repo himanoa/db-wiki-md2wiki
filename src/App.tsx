da2ce71288756f2eadb228d91fca066db988c44d
himanoa <matsunoappy@gmail.com>
2024-08-16 11:51:08 +0900

    Use wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { MainPage } from './pages/Main'
+import { Route, Router, Switch } from 'wouter'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <MainPage />
+      <Router base="/db-wiki-md2wiki">
+        <Switch>
+          <Route path="/"><MainPage /></Route>
+        </Switch>
+      </Router>
     </QueryClientProvider>
   )
 }
